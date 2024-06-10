052ad90a1dbb1b8ee5973ff2e04537e5845c6578
ManideepArruri <118426958+ManideepArruri@users.noreply.github.com>
2024-06-10 09:41:10 +0530

    Update selenium.js

--- selenium.js
+++ selenium.js
@@ -1,19 +1,20 @@
 
 const { Builder, By, Key, until } = require('selenium-webdriver');
 
-(async function example() {
+async function example() {
   let driver = await new Builder().forBrowser('chrome').build();
   try {
     await driver.get('http://www.google.com');
-
-    const searchBox = await driver.findElement(By.id('id="identifierId"'));
-    await searchBox.sendKeys('karthikmanimaheshuni@gmail.com', Key.RETURN);
+    let title = await driver.getTitle();
+    // const searchBox = await driver.findElement(By.id('id="identifierId"'));
+    // await searchBox.sendKeys('karthikmanimaheshuni@gmail.com', Key.RETURN);
     
-    await driver.wait(until.titleIs("gmail"), 5000);
+    // await driver.wait(until.titleIs("gmail"), 5000);
   } finally {
     //await driver.quit();
   }
-})();
+}
+example;
 
 
 //Make sure you have installed the necessary dependencies for Selenium WebDriver in your project using npm or yarn. You can run this script using Node.js.
